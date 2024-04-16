940a865ea364ba54718aef3f187f43b0676abc8e
angellllk <angellsrp98@gmail.com>
2024-04-16 12:13:49 +0300

    typo

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "/lspd-bbcoder/"
+  base: "/lspd-bbcoder/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
